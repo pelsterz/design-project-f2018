361ee669141d2aa6cc839b092ef0491845fec149
Andrew Victor <43546709+andrewvictor5@users.noreply.github.com>
2018-11-29 14:56:45 -0800

    Add files via upload

--- ps2_keyboard.sv
+++ ps2_keyboard.sv
@@ -4,55 +4,72 @@ module ps2_keyboard(
 	output logic [7:0] data_out
 );
 
-	logic [7:0] current_data;
-	logic [7:0] previous_data;
-	logic [3:0] count;
-	logic flag;
-	
-	initial
-	begin
-		count <= 4'h1;
-		flag <= 1'b0;
-		current_data <= 8'h0;
-		previous_data <= 8'h0;
-		data_out <= 8'h0;
-	end
-	
-	always_ff @(negedge clock)
-		begin
-			case(count)
-				1:; //First bit
-				2: current_data[0] <= data;
-				3: current_data[1] <= data;
-				4: current_data[2] <= data;
-				5: current_data[3] <= data;
-				6: current_data[4] <= data;
-				7: current_data[5] <= data;
-				8: current_data[6] <= data;
-				9: current_data[7] <= data;
-				10: flag <= 1'b1; //Parity bit
-				11: flag <= 1'b0; //Ending bit
-			
-			endcase
-			
-			if(count <= 10) //Check to see if count needs to be reset
-				count <= count + 1; //Increment count
-				
-			else if(count == 11) //Check to see if count needs to be reset
-				count <= 1; //Reset count
-				
-			end
-			
-		always_ff @(posedge flag) //Printing data 
+	logic [3:0] count = 4'b0000; //Counts number of clock cycles
+	logic parityCheck; 
+	logic [10:0] temp_data = 11'b00000000000; //Holds data
+
+		always_ff @(negedge clock)
 			begin
-				if(current_data == 8'h0)
-					data_out <= previous_data;
 				
+				//Shift data value into the temp register
+				if(clock)
+					begin
+						count <= 0;
+					end
+				
+				//Case statement that loads output and increments counter
 				else
-					previous_data <= current_data;
+				
+					begin
+						case(count)
+							11: 
+								
+								begin
+									temp_data <= {temp_data[9:0], data};
+									count <= 1;
+								end
+							
+							default:
+						
+								begin
+									count <= count + 1;
+									temp_data <= {temp_data[9:0], data};
+								end
+				
+						endcase
 					
+					end
+				
 			end
-			
+		
+			//Set output after counter is complete and data is valid
+			always_comb 
+				
+				begin
+					//Set parity check
+					parityCheck = ^temp_data[9:1];
+					
+					if(temp_data[0] && ~temp_data[10] && parityCheck && count >= 10)
+					
+						begin
+							data_out[0] = temp_data[9];
+							data_out[1] = temp_data[8];
+							data_out[2] = temp_data[7];
+							data_out[3] = temp_data[6];
+							data_out[4] = temp_data[5];
+							data_out[5] = temp_data[4];
+							data_out[6] = temp_data[3];
+							data_out[7] = temp_data[2];
+						end
+						
+					else
+						
+						begin
+							data_out = 8'b00000000;
+						end
+				
+				end
+				
 endmodule
 			
 			
